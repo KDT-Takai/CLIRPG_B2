70d2a3c9a181b63d80d0f3ce78d7592e2f6d0356
302-10\koyo <kobe-license@kobe-tech.ac.jp>
2026-01-20 18:08:55 +0900

    InitializeとFinalizeの仮想関数に変更(修正)

--- Program/System/IScene/IScene.hpp
+++ Program/System/IScene/IScene.hpp
@@ -8,13 +8,13 @@ public:
 	virtual ~IScene() = default;
 
 	// そのシーンに入った瞬間（タイトル画面表示、ステータス初期化等）
-	virtual void Initialize() {};
+	virtual void Initialize() {}
 
 	//　ループ毎に呼ばれる（入力待ち、コマンド処理等）
 	virtual void Update() = 0;
 
 	//そのシーンを抜ける瞬間（後始末、結果表示、フラグリセット等）
-	virtual void Finalize() {};
+	virtual void Finalize() {}
 
 	//　デバッグ用
 	virtual std::string GetName() const = 0;
