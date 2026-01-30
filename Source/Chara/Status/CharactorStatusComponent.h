869e8e61f5faca62ae5d02af3c8efd3e60573083
302-15\koyo <kobe-license@kobe-tech.ac.jp>
2026-01-30 17:52:01 +0900

    途中です また続きします 確認待ってください

--- Source/Chara/Status/CharactorStatusComponent.h
+++ Source/Chara/Status/CharactorStatusComponent.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <windows.h>
 
 namespace Charactors
 {
@@ -26,24 +27,39 @@ namespace Component
 	struct CharactorStatusComp
 	{
 		std::string Name = "Default";
-		Charactors::Status FixedStatus;
-		Charactors::Status CurrentStasuts;
-		Charactors::Level Level;
+		Charactors::Status BaseStatus;			//	基礎ステータス値
+		Charactors::Status FinalStasuts;		//	最終ステータス値
+		Charactors::Level Level;				//	レベル
+		int hp = -1;							//	体力値
 
 		CharactorStatusComp() = default;
 
 		CharactorStatusComp(const std::string& aName, const Charactors::Status& aStatus)
-			:Name(aName), FixedStatus(aStatus), CurrentStasuts(aStatus)
+			:Name(aName), BaseStatus(aStatus), FinalStasuts(aStatus),hp(FinalStasuts.HP)
 		{
 		}
 		
 
 		CharactorStatusComp(const std::string& aName, const Charactors::Status& aStatus,
 			const Charactors::Level& aLevel)
-			:Name(aName), FixedStatus(aStatus), CurrentStasuts(aStatus),Level(aLevel)
+			:Name(aName), BaseStatus(aStatus), FinalStasuts(aStatus),Level(aLevel), hp(FinalStasuts.HP)
 		{
 		}
 
+		const int GetMaxHP()
+		{
+			return FinalStasuts.HP;
+		}
+
+		const int GetHP()
+		{
+			return hp;
+		}
+
+		const void Heal(const int value)
+		{
+			hp = std::max(hp+value,)
+		}
 	};
 
 }
