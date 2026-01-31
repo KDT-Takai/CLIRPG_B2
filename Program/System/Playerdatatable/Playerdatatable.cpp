d7459cb24fb9630ee12c65379c85674900383d5e
KDT-Jolley <KDT24SGC40004@edu.kobe-tech.ac.jp>
2026-01-31 18:14:30 +0900

    修正完了

--- Program/System/Playerdatatable/Playerdatatable.cpp
+++ Program/System/Playerdatatable/Playerdatatable.cpp
@@ -1,24 +1,2 @@
 #include "Playerdatatable.h"
 
-//　プレイヤーデータの関数
-void Charactors::PlayerDataTable::InitPlayer() {
-    //　プレイヤーデータテーブルの作成
-    PlayerDataTable table;
-
-    //　プレイヤーのステータス
-    Status status = { 100,50,20,10 };
-    //　プレイヤーのレベル
-    Level level = { 1,0,100 };
-	
-    
-    //　プレイヤーの取得と表示
-    PlayerData* player = table.GetPlayer(0);
-    if (player) {
-        std::cout << player->name << " Lv." << player->level.level << std::endl;
-        std::cout << "HP: " << player->status.hp << std::endl;
-        std::cout << "MP: " << player->status.mp << std::endl;
-        std::cout << "攻撃力: " << player->status.attack << std::endl;
-        std::cout << "防御力: " << player->status.defense << std::endl;
-
-    }
-}
--- Program/System/Playerdatatable/Playerdatatable.h
+++ Program/System/Playerdatatable/Playerdatatable.h
@@ -5,33 +5,12 @@ namespace Charactors {
     // プレイヤーデータテーブルクラス
     class PlayerDataTable {
     public:
-        // ステータス構造体
-        static struct Status {
-            int hp;         //　hp
-            int mp;         //  mp
-            int attack;     // 攻撃力
-            int defense;    // 防御力
-        };
-
-        // レベル構造体
-        static struct Level {
-            int level;      // レベル
-            int exp;        // 経験値
-            int nextExp;    // 次のレベルまでの経験値
-        };
 
         //　プレイヤーデータの構造体
         static struct PlayerData {
             int id;                 // 0から始まるID
             std::string name;       // 名前
-            Status status;          // ステータス
-            Level level;            // レベル情報
         };
-        // プレイヤーの追加
-        void AddPlayer(const std::string& name, const Status& status, const Level& level) {
-            int id = static_cast<int>(mPlayer.size());
-            mPlayer.push_back({ id, name, status, level });
-        }
 
         //　プレイヤーのIDでの取得
         PlayerData* GetPlayer(int id) {
