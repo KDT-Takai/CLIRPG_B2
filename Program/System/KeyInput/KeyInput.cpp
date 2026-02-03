b3e382ede3a8e8612b78cfb8a4f65ce718c990b5
KDT-Jolley <KDT24SGC40004@edu.kobe-tech.ac.jp>
2026-02-03 17:38:09 +0900

    コメント追加

--- Program/System/KeyInput/KeyInput.cpp
+++ Program/System/KeyInput/KeyInput.cpp
@@ -1,9 +1,10 @@
 #include "KeyInput.h"
 namespace System {
-      
+    // キー入力の状態を保持する配列
     std::array<bool, KeyInput::KEY_NUM> KeyInput::mNow = {};
     std::array<bool, KeyInput::KEY_NUM> KeyInput::mPrev = {};
-    void System::KeyInput::Update() {
+    // キー入力の更新処理
+    void KeyInput::Update() {
         mPrev = mNow;
         for (int i = 0; i < KEY_NUM; i++) {
             mNow[i] = (GetAsyncKeyState(i) & 0x8000) != 0;
@@ -11,12 +12,12 @@ namespace System {
     }
 
     //　指定キーが押されているか
-    bool System::KeyInput::IsPress(int aKey) {
+    bool KeyInput::IsPress(int aKey) {
         if (aKey < 0 || aKey >= KEY_NUM)return false;
         return mNow[aKey];
     }
     //　指定キーが押された瞬間か
-    bool System::KeyInput::IsDown(int aKey) {
+    bool KeyInput::IsDown(int aKey) {
         if (aKey < 0 || aKey >= KEY_NUM)return false;
         return mNow[aKey] && !mPrev[aKey];
     }
